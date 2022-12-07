c68856ec41808e46f8441ff7234bfcfb710b9427
anilovr <dotlaudo@gmail.com>
2022-12-07 16:36:53 -0300

    Bot base: based on DJS Guide

---
+++ main.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+require("dotenv").config();
+
+const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+bot.commands = new Collection();
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		bot.once(event.name, (...args) => event.execute(...args));
+	} else {
+		bot.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		bot.commands.set(command.data.name, command);
+	} else {
+		console.log(`[W] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+bot.login(process.env.TOKEN);
